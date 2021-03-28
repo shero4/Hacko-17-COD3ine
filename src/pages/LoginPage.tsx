7417456ed20f5c3ab03e0812c5a8134499321cfa
shero4 <goyal.dhruva@gmail.com>
2021-03-28 10:47:41 +0530

    yelo

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -10,14 +10,14 @@ import { useDispatch } from 'react-redux';
 
 
 const LoginPage: React.FC = () => {
-    const [busy, setBusy] = useState<boolean>(false)
-    const [email, setEmail] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
-    const [message, setMessage] = useState<string>('')
-    const [showToast, setShowToast] = useState<boolean>(false)
+  const [busy, setBusy] = useState<boolean>(false)
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [message, setMessage] = useState<string>('')
+  const [showToast, setShowToast] = useState<boolean>(false)
 
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
   const history = useHistory()
 
   async function login() {
@@ -39,57 +39,50 @@ const LoginPage: React.FC = () => {
 
 
   return (
-    
+
     <IonPage >
-        
+
       <IonLoading
         message="please wait"
         duration={0}
         isOpen={busy}
       />
-      <IonContent id="background-image-setter" fullscreen>
-
-          <div className="ion-text-center login-form-main-grid">
-  
-                <div className='take-full-width'>
-                <h1 className='login-heading'>Log-In</h1>
-                </div>
-                <div className='take-full-width'>
-
-                <IonInput onIonChange={
-                    (e:any)=>{
-                        setEmail(e.target.value);
-                    }
-                } placeholder="Email" required={true} type='email' className='input-styling'  />
-                </div>
-                <div className='take-full-width'>
-                <IonInput onIonChange={
-                    (e:any)=>{
-                        setPassword(e.target.value);
-                    }
-                } placeholder='Password' required={true} type='password' className='input-styling'  />
-                </div>
-                <div className='take-full-width'>
-                <IonButton onClick={login} shape='round' expand='block' color='primary' id='login-main-button' size='large'>Sign In</IonButton>
-                </div>
-                <div className="take-full-width">
-                    {/* <IonText color='dark' > */}
-                        <p id='sign-up-text'>
-                        Don't have an account?<Link to="/register">Register</Link>
-                        </p>
-                        
-                        
-                        {/* </IonText> */}
-                </div>
-                  
-        
-    
-                
-
-                
-          
+      <IonContent fullscreen className="ion-padding">
+        <div className="ion-text-center login-form-main-grid">
+          <div className='take-full-width'>
+            <h1 className='login-heading'>Log-In</h1>
           </div>
-          <IonToast
+          <div className='take-full-width'>
+            <IonInput onIonChange={
+              (e: any) => {
+                setEmail(e.target.value);
+              }
+            } placeholder="Email" required={true} type='email' className='input-styling' />
+          </div>
+          <div className='take-full-width'>
+            <IonInput onIonChange={
+              (e: any) => {
+                setPassword(e.target.value);
+              }
+            } placeholder='Password' required={true} type='password' className='input-styling' />
+          </div>
+          <div className='take-full-width'>
+            <IonButton onClick={login} shape='round' expand='block' color='primary' id='login-main-button' size='large'>Sign In</IonButton>
+          </div>
+          <div className="take-full-width">
+            <p id='sign-up-text'>
+              Don't have an account? <Link to="/register">Register</Link>
+            </p>
+          </div>
+
+
+
+
+
+
+
+        </div>
+        <IonToast
           isOpen={showToast}
           onDidDismiss={() => setShowToast(false)}
           message={message}
@@ -97,11 +90,11 @@ const LoginPage: React.FC = () => {
         />
 
 
-        
-       
+
+
       </IonContent>
-   
-  </IonPage>
+
+    </IonPage>
   );
 };
 
--- src/pages/PreLogin.tsx
+++ src/pages/PreLogin.tsx
@@ -1,45 +1,45 @@
-import { IonButton, IonButtons,IonRow,IonCol,IonGrid, IonContent,IonImg,IonText, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonRow, IonCol, IonGrid, IonContent, IonImg, IonText, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './PreLogin.css'
 
 
 
 const PreLogin: React.FC = () => {
-  const mainLogoSource : string = 'assets/icon/main-logo.png';
+  const mainLogoSource: string = 'assets/icon/main-logo.png';
   return (
-   
-  
-    
-      
-      <IonPage >
-        <IonContent id="background-image-section" >
-             
-             <IonImg id="main-logo-styling" src={mainLogoSource}/>
-  
-             
-        </IonContent>
-        <IonContent id="buttons-section" >
-          <IonGrid>
-            <IonRow>
-          <IonCol>
-            <div>
-            <IonButton  class="explore-more-button"  shape='round'>Sign Up</IonButton>
-            </div>
 
-          
-          </IonCol>
-          <IonCol>
-            <div>
-            <IonButton class="explore-more-button"  shape='round'>Log In</IonButton>
-            </div>
-          
-          </IonCol>
-            </IonRow>
-          
-          
-          </IonGrid>
 
-        </IonContent>
-     
+
+
+    <IonPage >
+      <IonContent id="background-image-section" >
+
+        <IonImg id="main-logo-styling" src={mainLogoSource} />
+
+
+      </IonContent>
+      <IonContent id="buttons-section" >
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <div>
+                <IonButton class="explore-more-button" shape='round' routerLink="/register">Sign Up</IonButton>
+              </div>
+
+
+            </IonCol>
+            <IonCol>
+              <div>
+                <IonButton class="explore-more-button" shape='round' routerLink="/login">Log In</IonButton>
+              </div>
+
+            </IonCol>
+          </IonRow>
+
+
+        </IonGrid>
+
+      </IonContent>
+
     </IonPage>
   )
 };
