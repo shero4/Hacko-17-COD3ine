407cd5a89260589a3829b500b86260227aa542d2
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-03-28 10:00:38 +0530

    Merged all

--- src/pages/PreLogin.tsx
+++ src/pages/PreLogin.tsx
@@ -1,20 +1,47 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons,IonRow,IonCol,IonGrid, IonContent,IonImg,IonText, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import './PreLogin.css'
+
+
 
 const PreLogin: React.FC = () => {
-
+  const mainLogoSource : string = 'assets/icon/main-logo.png';
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>PreLogin</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <IonButton routerLink="/login">Login</IonButton>
-        <IonButton routerLink="/register" color="secondary">Register</IonButton>
-      </IonContent>
+   
+  
+    
+      
+      <IonPage >
+        <IonContent id="background-image-section" >
+             
+             <IonImg id="main-logo-styling" src={mainLogoSource}/>
+  
+             
+        </IonContent>
+        <IonContent id="buttons-section" >
+          <IonGrid>
+            <IonRow>
+          <IonCol>
+            <div>
+            <IonButton  class="explore-more-button"  shape='round'>Sign Up</IonButton>
+            </div>
+
+          
+          </IonCol>
+          <IonCol>
+            <div>
+            <IonButton class="explore-more-button"  shape='round'>Log In</IonButton>
+            </div>
+          
+          </IonCol>
+            </IonRow>
+          
+          
+          </IonGrid>
+
+        </IonContent>
+     
     </IonPage>
-  );
+  )
 };
 
 export default PreLogin;
